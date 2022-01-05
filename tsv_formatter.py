e1fb697625d57df91319143c20b5a284c51c39d6
yztxwd <yztxwd@gmail.com>
2022-01-04 23:13:44 -0500

    Add formatter script to handle meta info from sra-explorer

---
+++ tsv_formatter.py
@@ -0,0 +1,40 @@
+#!python3
+
+description = """
+
+    Accepts tsv meta info from sra-explorer(https://sra-explorer.info), format it into samples.tsv and downloads.tsv for pipeline
+    
+    Usage:
+		
+        $ python tsv_formatter.py meta.tsv
+
+"""
+
+import sys
+import pandas as pd
+
+def main():
+    meta = pd.read_table(sys.argv[1], sep="\t")
+    
+    # downloads.tsv
+    downloads = meta[["FastQ filename", "FastQ Aspera URL"]]
+    downloads.columns = ["fq", "url"]
+    downloads.to_csv("downloads.tsv", header=True, index=False, sep="\t")
+    
+    # samples.tsv
+    samples = meta.groupby("Title").apply(lambda chunk: pd.Series(chunk["FastQ filename"].to_list())).reset_index()
+    if samples.shape[1] == 2:
+        # single end
+        samples.columns = ["sample", "fq1"]
+        samples["fq2"] = None
+    else:
+        # pair end
+        samples.columns = ["sample", "fq1", "fq2"]
+    samples['condition'] = "WT"
+    samples['rep'] = "rep1"
+    samples['unit'] = "tech1"
+    samples = samples.loc[:,["sample", "condition", "rep", "unit", "fq1", "fq2"]]
+    samples.to_csv("samples.tsv", header=True, index=False, sep="\t")
+
+if __name__ == "__main__":
+    main()
